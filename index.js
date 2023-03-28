82be526ff9f99f1ded95898aced3ec78e763f318
Hushibul <nayeemninz786@gmail.com>
2023-03-28 14:47:17 +0600

    sending file

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const pathLocation = path.resolve();
+  res.sendFile(path.join(pathLocation, "./index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server is runnning");
+});
